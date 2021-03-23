35a9e4c5db8dfead431427caabbe24af72572cf3
sega <sergeykinn@mail.ru>
2021-03-23 19:02:18 +0300

    4. candicates.jsp - список кандидатов. [#456710]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -14,9 +14,9 @@ public class Store {
 
     private static final Store INST = new Store();
 
-    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
-    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));

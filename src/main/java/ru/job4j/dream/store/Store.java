021654b20bf167652c39cda47d1b66493661427c
sega <sergeykinn@mail.ru>
2021-03-24 18:34:32 +0300

    2. CandidateSerlvet. Создание кандидата. [#456720]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -51,7 +51,7 @@ public class Store {
 
     public void save(Candidate candidate) {
         if (candidate.getId() == 0) {
-            candidate.setId(POST_ID.incrementAndGet());
+            candidate.setId(CANDIDATE_ID.incrementAndGet());
         }
         candidates.put(candidate.getId(), candidate);
     }

7793d9ba85ffc637b329df5fc695bb14301c78a8
sega <sergeykinn@mail.ru>
2021-03-27 12:44:59 +0300

    1. PsqlStore. [#456689]

---
+++ src/main/java/ru/job4j/dream/store/PsqlMain.java
@@ -0,0 +1,21 @@
+package ru.job4j.dream.store;
+
+
+import ru.job4j.dream.model.Post;
+
+public class PsqlMain {
+    public static void main(String[] args) {
+        Store store = PsqlStore.instOf();
+        store.save(new Post(0, "Java Job"));
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+
+        store.save(new Post(1, "Java Job22222222222222"));
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+
+        System.out.println(store.findById(1).getName());
+    }
+}
